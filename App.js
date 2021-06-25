0d4d6ede90ca316b3dc3dc2326b081fa8c2173c1
Nicolas Posa <nicoposa57@gmail.com>
2021-06-25 15:22:26 -0300

    use Context for themes

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useContext } from "react";
 
-function HookProblem() {
-  const [darkMode, setDarkMode] = useState(false);
-  const styles = useStyles(darkMode);
+import Card from "./card/Card";
+import { ThemeProvider } from "./theme-context/theme-context.js";
 
+const App = () => {
   return (
-    <View style={styles.con}>
-      <Text style={styles.text}>
-        {darkMode ? "That's Dark Mode" : "That's Light Mode"}
-      </Text>
-      <Button title="Dark Mode" onPress={() => setDarkMode(true)} />
-      <Button title="Light Mode" onPress={() => setDarkMode(false)} />
-    </View>
+    <ThemeProvider>
+      <Card />
+    </ThemeProvider>
   );
-}
+};
 
-function useStyles(darkMode) {
-  return StyleSheet.create({
-    con: {
-      backgroundColor: darkMode ? "black" : "#ffd",
-      flex: 1,
-      justifyContent: "center",
-    },
-    text: {
-      textAlign: "center",
-      color: darkMode ? "white" : "black",
-      paddingBottom: 15,
-    },
-  });
-}
-
-export default HookProblem;
+export default App;
